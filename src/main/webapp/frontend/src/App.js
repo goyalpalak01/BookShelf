ead3222b7d461cc48d41c34d1de1976505b96132
shubhi2206 <maheshwarishubhi22@gmail.com>
2021-04-12 17:27:15 +0530

    Welcome Page Completed

--- src/main/webapp/frontend/src/App.js
+++ src/main/webapp/frontend/src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import welcome from "./components/welcome"
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>
-        Hello World
-      </h1>
-    </div>
-  );
+class App extends Component{
+    render() {
+        return (
+            <Router>
+                <Route exact path = '/' component={welcome}>
+                </Route>
+                <Route exact path = '/SignIn' component={SignIn}>
+                </Route>
+                <Route exact path = '/SignUp' component={SignUp}>
+                </Route>
+            </Router>
+        )
+    }
 }
+
+
+
 
 export default App;
---
+++ src/main/webapp/frontend/src/components/welcome.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+
+class welcome extends Component {
+    constructor(props) {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(){
+        this.props.history.push('/SignUp');
+    }
+
+    render() {
+        return (
+            <div className= "Welcome">
+                <body>
+                <div className="content">
+                    <h1>A New Way to Organize your Bookmarks</h1>
+                    <p>BookShelf is the best platform to help you organize your bookmarks into multiple categories
+                        and allows the user to add and save tags to their articles.</p>
+                    <button onClick={this.handleSubmit} className="createAccount">Create Account ></button>
+                </div>
+                <nav>
+                    <input type="checkbox" id="check"/>
+                    <label htmlFor="check" className="checkBtn">
+                        <i className="fas fa-bars"/>
+                    </label>
+                    <label className="logo">BookShelf</label>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/SignIn">About</Link></li>
+                        <li><Link to="/SignIn">Sign In</Link></li>
+                        <li><Link to="/SignIn">Sign Up</Link></li>
+                        <li className="createAccountShort"><Link to="/SignUp">Create Account</Link></li>
+                    </ul>
+                </nav>
+                </body>
+            </div>
+        )
+    }
+
+}
+export default welcome
